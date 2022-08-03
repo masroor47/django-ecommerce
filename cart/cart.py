db44774dd5bc7632f0e61f18fd26ad78d331dc89
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-08-03 15:25:40 -0400

    add cart class and context processor

---
+++ cart/cart.py
@@ -0,0 +1,13 @@
+
+
+
+
+class Cart():
+    # Default behaviours that can be inherited or overrided
+
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('skey')
+        if 'skey' not in request.session:
+            cart = self.session['skey'] = {}
+        self.cart = cart
